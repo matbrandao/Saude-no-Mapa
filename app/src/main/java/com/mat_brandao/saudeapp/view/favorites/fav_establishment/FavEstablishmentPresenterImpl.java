498cab38ba76fa4d3aecd0e4e1a90d1df6b0ebf0
mat_brandao <mat_brandao@live.com>
2016-09-28 14:58:18 -0300

    Added connections errors to FavEstablishment

--- app/src/main/java/com/mat_brandao/saudeapp/view/favorites/fav_establishment/FavEstablishmentPresenterImpl.java
+++ app/src/main/java/com/mat_brandao/saudeapp/view/favorites/fav_establishment/FavEstablishmentPresenterImpl.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.util.Log;
 import android.view.View;
 
+import com.mat_brandao.saudeapp.R;
 import com.mat_brandao.saudeapp.domain.model.Conteudo;
 import com.mat_brandao.saudeapp.domain.model.Establishment;
 import com.mat_brandao.saudeapp.domain.model.PostContent;
+import com.mat_brandao.saudeapp.domain.model.PostResponse;
 import com.mat_brandao.saudeapp.domain.util.GenericObjectClickListener;
 import com.mat_brandao.saudeapp.domain.util.GenericUtil;
 
@@ -19,6 +21,7 @@ import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.subscriptions.CompositeSubscription;
 
+
 public class FavEstablishmentPresenterImpl implements FavEstablishmentPresenter, GenericObjectClickListener<Establishment> {
     private static final String TAG = "FavEstablishmentPresent";
 
@@ -28,6 +31,9 @@ public class FavEstablishmentPresenterImpl implements FavEstablishmentPresenter,
 
     private List<Long> mEstablishmentCodeList = new ArrayList<>();
     private List<Establishment> mEstablishmentList = new ArrayList<>();
+
+    private Observable mLastObservable;
+    private Observer mLastObserver;
 
     private CompositeSubscription mSubscription = new CompositeSubscription();
 
@@ -49,7 +55,10 @@ public class FavEstablishmentPresenterImpl implements FavEstablishmentPresenter,
 
     @Override
     public void onRetryClicked() {
-        // TODO: 28/09/2016
+        showProgressBar();
+        mLastObservable
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(mLastObserver);
     }
 
     public FavEstablishmentPresenterImpl(FavEstablishmentView view, Context context) {
@@ -61,28 +70,12 @@ public class FavEstablishmentPresenterImpl implements FavEstablishmentPresenter,
     }
 
     private void requestFavEstablishments() {
+        mLastObservable = mInteractor.requestGetUserPosts();
+        mLastObserver = postResponseObserver;
         mSubscription.add(mInteractor.requestGetUserPosts()
                 .observeOn(AndroidSchedulers.mainThread())
                 .onErrorReturn(throwable -> null)
-                .subscribe(listResponse -> {
-                    if (listResponse != null && listResponse.isSuccessful()) {
-                        if (listResponse.body() != null && listResponse.body().size() > 0) {
-                            mInteractor.saveUserLikePostCode(listResponse.body().get(0).getCodPostagem());
-                            List<Long> codConteudoList = new ArrayList<>();
-                            for (Conteudo conteudo : listResponse.body().get(0).getConteudos()) {
-                                codConteudoList.add(conteudo.getCodConteudoPostagem());
-                            }
-                            Observable.from(codConteudoList)
-                                    .flatMap(codConteudo -> mInteractor.requestGetPostContent(codConteudo))
-                                    .observeOn(AndroidSchedulers.mainThread())
-                                    .subscribe(postContentObserver);
-                        } else {
-                            // TODO: 28/09/2016 show error to user
-                        }
-                    } else {
-                        // TODO: 28/09/2016 show error to user
-                    }
-                }));
+                .subscribe(postResponseObserver));
     }
 
     @Override
@@ -105,9 +98,50 @@ public class FavEstablishmentPresenterImpl implements FavEstablishmentPresenter,
         mView.setEmptyViewVisibility(View.VISIBLE);
     }
 
+    private Observer<Response<List<PostResponse>>> postResponseObserver = new Observer<Response<List<PostResponse>>>() {
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            mView.showNoConnectionSnackBar();
+        }
+
+        @Override
+        public void onNext(Response<List<PostResponse>> listResponse) {
+            if (listResponse.isSuccessful()) {
+                if (listResponse.body() != null && listResponse.body().size() > 0) {
+                    mInteractor.saveUserLikePostCode(listResponse.body().get(0).getCodPostagem());
+                    List<Long> codConteudoList = new ArrayList<>();
+                    for (Conteudo conteudo : listResponse.body().get(0).getConteudos()) {
+                        codConteudoList.add(conteudo.getCodConteudoPostagem());
+                    }
+                    mLastObservable = Observable.from(codConteudoList)
+                            .flatMap(codConteudo -> mInteractor.requestGetPostContent(codConteudo));
+                    mLastObserver = postContentObserver;
+
+                    Observable.from(codConteudoList)
+                            .flatMap(codConteudo -> mInteractor.requestGetPostContent(codConteudo))
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(postContentObserver);
+                } else {
+                    showEmptyView();
+                }
+            } else {
+                mView.showToast(mContext.getString(R.string.http_error_generic));
+            }
+        }
+    };
+
     private Observer<Response<PostContent>> postContentObserver = new Observer<Response<PostContent>>() {
         @Override
         public void onCompleted() {
+            mLastObservable = Observable.from(mEstablishmentCodeList)
+                    .flatMap(establishmentCode -> mInteractor.requestGetEstablishment(establishmentCode));
+            mLastObserver = establishmentObserver;
+
             Observable.from(mEstablishmentCodeList)
                     .flatMap(establishmentCode -> mInteractor.requestGetEstablishment(establishmentCode))
                     .observeOn(AndroidSchedulers.mainThread())
@@ -116,12 +150,15 @@ public class FavEstablishmentPresenterImpl implements FavEstablishmentPresenter,
 
         @Override
         public void onError(Throwable e) {
+            mView.showNoConnectionSnackBar();
         }
 
         @Override
         public void onNext(Response<PostContent> postContentResponse) {
             if (postContentResponse.isSuccessful()) {
                 mEstablishmentCodeList.add(GenericUtil.getNumbersFromString(postContentResponse.body().getJSON()));
+            } else {
+                mView.showToast(mContext.getString(R.string.http_error_generic));
             }
         }
     };
@@ -139,7 +176,7 @@ public class FavEstablishmentPresenterImpl implements FavEstablishmentPresenter,
 
         @Override
         public void onError(Throwable e) {
-
+            mView.showNoConnectionSnackBar();
         }
 
         @Override
@@ -148,6 +185,8 @@ public class FavEstablishmentPresenterImpl implements FavEstablishmentPresenter,
                 if (listResponse.body() != null && listResponse.body().size() > 0) {
                     mEstablishmentList.add(listResponse.body().get(0));
                 }
+            } else {
+                mView.showToast(mContext.getString(R.string.http_error_generic));
             }
         }
     };
