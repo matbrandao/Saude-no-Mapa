b9b68bc3be655468d1bc59338be3453dee32cdc3
mat_brandao <mat_brandao@live.com>
2016-10-11 17:52:47 -0300

    Added firebase messaging to the app

---
+++ app/src/main/java/com/mat_brandao/saudeapp/domain/fcm/FirebaseMessasing.java
@@ -0,0 +1,29 @@
+package com.mat_brandao.saudeapp.domain.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Mateus Brandão on 11/10/2016.
+ */
+
+public class FirebaseMessasing extends FirebaseMessagingService {
+    private static final String TAG = "FirebaseMessasing";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+}
